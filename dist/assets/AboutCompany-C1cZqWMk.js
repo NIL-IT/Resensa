0d5ef05a280c3bcad6bbab53a4480e759feb2412
Danil <chyksin@bk.ru>
2025-02-27 20:27:56 +0700

    commit 55

--- dist/assets/AboutCompany-C1cZqWMk.js
+++ dist/assets/AboutCompany-C1cZqWMk.js
@@ -1,4 +1,4 @@
-import{j as t,r as n}from"./three-vendor-D76XtHXd.js";import{T as a,u as x,y as o,b as i,H as m}from"./index-Dg4eWNDb.js";import{E as c}from"./EquipmentBanner-C_Z5SDQ4.js";import{O as p}from"./Objects-Crnj-w8y.js";import{A as d}from"./Advantages-SIl_pDTs.js";import{S as l,s as u,a as b,b as g,c as j,P as h}from"./SliderPage-BF_DdJI3.js";import{F as f}from"./Footer-rsOP-Zs2.js";import"./react-vendor-C6O4aKhz.js";const T=[{title:`ПРЕДПРОДАЖНАЯ 
+import{j as t,r as n}from"./three-vendor-D76XtHXd.js";import{T as a,u as x,y as o,b as i,H as m}from"./index-BJ7rdYQL.js";import{E as c}from"./EquipmentBanner-C0oTVucS.js";import{O as p}from"./Objects-BpLuGvh1.js";import{A as d}from"./Advantages-CQvNW9vT.js";import{S as l,s as u,a as b,b as g,c as j,P as h}from"./SliderPage-Dy5D2YoS.js";import{F as f}from"./Footer-CKNLOfaX.js";import"./react-vendor-C6O4aKhz.js";const T=[{title:`ПРЕДПРОДАЖНАЯ 
 ПОДГОТОВКА`,text:`Подтверждаем максимальное соответствие
 параметров вентагрегатов проектным значениям.`},{title:"ОКРАСКА",subTitle:"В ЛЮБОЙ ЦВЕТ",text:`По желанию клиента мы окрашиваем
 вентиляционный агрегат в любой цвет
