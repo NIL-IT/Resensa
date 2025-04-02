559aab24d17d9e3a23acf58adea4485a125e3ddb
Danil <chyksin@bk.ru>
2025-04-02 18:31:55 +0700

    update banner

---
+++ dist/client/assets/Objects-DEwQ6eZx.js
@@ -0,0 +1,23 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/EarthScene-TBD0Ts1B.js","assets/index-_gwFs9sh.js","assets/react-vendor-DgitYnsx.js","assets/three-vendor-BPj0iCA8.js","assets/index-jNS6SGJ7.css"])))=>i.map(i=>d[i]);
+import{r,_ as a}from"./three-vendor-BPj0iCA8.js";import{j as e,T as p,B as c}from"./index-_gwFs9sh.js";const u=r.lazy(()=>a(()=>import("./EarthScene-TBD0Ts1B.js"),__vite__mapDeps([0,1,2,3,4]))),d=[{name:"Административные объекты",id:1},{name:"Производства",id:2},{name:"Медицина и фармацевтика",id:3}];function f({className:n="",about:o=!1}){const[t,x]=r.useState(0),m=s=>x(s);return e.jsxDEV("section",{className:`w-full bg-gray-400 py-12 sm:py-16 md:py-20 lg:py-24 ${o?"mt-[0px] xs:mt-[28px] md:mt-[160px] lg:mt-[225px] xl:mt-[205px] 2xl:mt-[220px] 3xl:mt-[225px]":""} ${n}`,children:e.jsxDEV("div",{className:"earth_container relative max-w-[full] min-h-[500px] sm:min-h-[550px] md:min-h-[550px] lg:min-h-[602px] xl:min-h-[650px] 2xl:min-h-[702px] flex flex-col justify-center",children:e.jsxDEV("div",{className:`flex-center flex-col \r
+        lg:flex-row lg:items-center lg:justify-between g\r
+        ap-20 lg:gap-12`,children:[e.jsxDEV("div",{className:` w-full  \r
+          md:h-[800px] lg:h-[1000px] xl:h-[auto]\r
+            \r
+          sm:h-[850px] \r
+            xs:h-[800px] \r
+         h-[800px] px-4 sm:px-6 lg:px-8`,children:[e.jsxDEV(p,{text:"наши объекты",className:"inline-block text-white text-xl sm:text-2xl lg:text-3xl border-b border-b-white mb-6"},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:38,columnNumber:13},this),e.jsxDEV("article",{className:" max-w-[411px]",children:[e.jsxDEV("div",{className:"text-white border border-white divide-y divide-white w-full",children:d.map(({name:s,id:i},l)=>e.jsxDEV("button",{type:"button",className:`select-none px-4 w-full text-left py-3 sm:py-4 lg:py-5 text-base sm:text-lg lg:text-xl cursor-pointer uppercase ${l===t?"text-gray-400 bg-white":""}`,onClick:()=>m(l),children:s},i,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:45,columnNumber:19},this))},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:43,columnNumber:15},this),e.jsxDEV(c,{target:"_blank",href:"https://yandex.ru/maps/65/novosibirsk/house/serebrennikovskaya_ulitsa_14/bEsYfwVnSk0EQFtvfXxzcXpkZw==/?ll=82.927382%2C55.020673&z=17",icon:!0,text:"построить маршрут до офиса",className:`bg-white text-gray-400 w-full select-none justify-center xl:justify-normal\r
+              mt-6 sm:mt-8 text-sm lg:text-md xl:text-base \r
+               font-normal \r
+              py-3 xl:py-5  px-4 sm:px-3 xl:px-5\r
+ \r
+               gap-5 xl:gap-10 xl:text-center 2xl:text-start`,white:!0},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:57,columnNumber:15},this)]},void 0,!0,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:42,columnNumber:13},this)]},void 0,!0,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:30,columnNumber:11},this),e.jsxDEV("div",{className:`absolute   \r
+            3xl:top-[40px] 3xl:right-[-250px]\r
+            2xl:top-[60px] 2xl:right-[-200px]\r
+            xl:top-[100px] xl:right-[-50px]\r
+            lg:top-[460px] lg:right-[-140px]\r
+            md:top-[360px] md:right-[-80px]\r
+            sm:top-[400px] sm:right-[5px]\r
+            xs:top-[360px] xs:right-[0px]\r
+            top-[360px] right-[0px] z-50\r
+       `,children:e.jsxDEV(r.Suspense,{fallback:null,children:e.jsxDEV(u,{index:t},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:85,columnNumber:15},this)},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:84,columnNumber:13},this)},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:72,columnNumber:11},this)]},void 0,!0,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:25,columnNumber:9},this)},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:24,columnNumber:7},this)},void 0,!1,{fileName:"C:/Users/user/Desktop/Code/Work/resensa/src/components/shared/Objects.jsx",lineNumber:17,columnNumber:5},this)}export{f as O};
--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -19,68 +19,78 @@ export default function Banner({ banner }) {
   }, []);
   return (
     <section
-      className="pl-5 xs:pl-0 container 
+      className="pl-5 xs:pl-0 
       pt-[40px] xs:pt-[45px] sm:pt-[50px] md:pt-[90px] lg:pt-[118px] xl:pt-[120px] 2xl:pt-[122px] 3xl:pt-[125px] 
-    pb-[35px] xs:pb-[40px] sm:pb-[45px] md:pb-[75px] lg:pb-[102px] xl:pb-[105px] 2xl:pb-[108px] 3xl:pb-[110px]
+    
     "
     >
-      <MainTitle
-        className="border-b border-gray-400 pb-[20px] xs:pb-[23px] sm:pb-[25px] md:pb-[28px] 
+      <div className="container ">
+        <MainTitle
+          className="border-b border-gray-400 pb-[20px] xs:pb-[23px] sm:pb-[25px] md:pb-[28px] 
         lg:pb-[30px] xl:pb-[31px] 2xl:pb-[32px] 3xl:pb-[33px]
         text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-[48px] sm:leading-[40px] md:leading-[50px] lg:leading-[72px]"
-        text="производство и поставка вентиляционного оборудования высокого класса"
-      />
-      <div className="flex-center flex-col lg:flex-row justify-between mt-3 xs:mt-3.5 sm:mt-4 md:mt-4 lg:mt-4 xl:mt-4 2xl:mt-4 3xl:mt-4 gap-6 xs:gap-7 sm:gap-8 lg:gap-4">
-        <div className="flex flex-wrap gap-6 xs:gap-7 sm:gap-8 md:gap-[60px] lg:gap-[70px] xl:gap-[75px] 2xl:gap-[80px] 3xl:gap-[88px]">
-          <div className=" xs:gap-5 block md:w-[290px] xl:w-[312px]">
-            <span
-              className="text-[28px] xs:text-[30px] sm:text-[32px] md:text-[42px] lg:text-[44px] xl:text-[45px] 2xl:text-[46px] 
+          text="производство и поставка вентиляционного оборудования высокого класса"
+        />
+        <div className="flex-center flex-col lg:flex-row justify-between mt-3 xs:mt-3.5 sm:mt-4 md:mt-4 lg:mt-4 xl:mt-4 2xl:mt-4 3xl:mt-4 gap-6 xs:gap-7 sm:gap-8 lg:gap-4">
+          <div className="flex flex-wrap gap-6 xs:gap-7 sm:gap-8 md:gap-[60px] lg:gap-[70px] xl:gap-[75px] 2xl:gap-[80px] 3xl:gap-[88px]">
+            <div className=" xs:gap-5 block md:w-[290px] xl:w-[312px]">
+              <span
+                className="text-[28px] xs:text-[30px] sm:text-[32px] md:text-[42px] lg:text-[44px] xl:text-[45px] 2xl:text-[46px] 
             3xl:text-[48px] text-gray-400 min-w-[auto] sm:min-w-[300px]"
-            >
-              {banner.first_value || "22"}
-            </span>
-            <p
-              className="text-base xs:text-base sm:text-lg md:text-xl lg:text-xl xl:text-xl 2xl:text-xl 3xl:text-xl
+              >
+                {banner.first_value || "22"}
+              </span>
+              <p
+                className="text-base xs:text-base sm:text-lg md:text-xl lg:text-xl xl:text-xl 2xl:text-xl 3xl:text-xl
              text-gray-300 xs:text-start text-left "
-            >
-              {banner.first_value_string ||
-                `года опыта на рынке
+              >
+                {banner.first_value_string ||
+                  `года опыта на рынке
 вентиляционного оборудования`}
-            </p>
-          </div>
-          <div className="block xs:gap-5  md:w-[280px] xl:w-[312px]">
-            <span
-              className="text-[28px] xs:text-[30px] sm:text-[32px] 
+              </p>
+            </div>
+            <div className="block xs:gap-5  md:w-[280px] xl:w-[312px]">
+              <span
+                className="text-[28px] xs:text-[30px] sm:text-[32px] 
             md:text-[42px] lg:text-[44px] xl:text-[45px] 2xl:text-[46px] 
             3xl:text-[48px] text-gray-400 xs:w-[34px] sm:w-[36px] md:w-[48px] lg:w-[50px] xl:w-[auto] min-w-[auto] sm:min-w-[300px]"
-            >
-              {banner.second_value || `3`}
-            </span>
-            <p
-              className="text-base xs:text-base sm:text-lg md:text-xl lg:text-xl
+              >
+                {banner.second_value || `3`}
+              </span>
+              <p
+                className="text-base xs:text-base sm:text-lg md:text-xl lg:text-xl
              xl:text-xl 2xl:text-xl 3xl:text-xl text-gray-300 xs:text-start text-left"
-            >
-              {banner.second_value_string ||
-                `года гарантии
+              >
+                {banner.second_value_string ||
+                  `года гарантии
 на продукцию компании.`}
-            </p>
+              </p>
+            </div>
+          </div>
+          <div className="mr-5 xs:mr-0">
+            <Button
+              onClick={() => handleChangeShowPopup(true)}
+              className=" py-[16px] xs:py-[17px] sm:py-[18px] md:py-[26px] lg:py-[26px] xl:py-[26px] 2xl:py-[26px] 3xl:py-[26px] px-4 xs:px-5 sm:px-6 md:pr-[26px] md:pl-6 w-full xs:w-full sm:w-full md:w-[332px] h-[56px] xs:h-[58px] sm:h-[60px] md:h-[76px] lg:h-[76px] xl:h-[76px] 2xl:h-[76px] 3xl:h-[76px] text-sm xs:text-sm sm:text-base"
+              icon={true}
+              text={"Получить консультацию"}
+            />
           </div>
         </div>
-        <div className="mr-5 xs:mr-0">
-          <Button
-            onClick={() => handleChangeShowPopup(true)}
-            className=" py-[16px] xs:py-[17px] sm:py-[18px] md:py-[26px] lg:py-[26px] xl:py-[26px] 2xl:py-[26px] 3xl:py-[26px] px-4 xs:px-5 sm:px-6 md:pr-[26px] md:pl-6 w-full xs:w-full sm:w-full md:w-[332px] h-[56px] xs:h-[58px] sm:h-[60px] md:h-[76px] lg:h-[76px] xl:h-[76px] 2xl:h-[76px] 3xl:h-[76px] text-sm xs:text-sm sm:text-base"
-            icon={true}
-            text={"Получить консультацию"}
-          />
-        </div>
       </div>
-      <div className="mr-5 xs:mr-0 flex justify-center">
+
+      <div className=" flex justify-center">
         <img
           className={`${imageSrc === imgPlaceholder ? "loading" : "loaded"}
-          w-[80%] mt-[35px] xs:mt-[40px] sm:mt-[45px] md:mt-[75px] lg:mt-[85px] 
+          min-w-[100wh] max-h-[700px]  mt-[35px] xs:mt-[40px] sm:mt-[45px] md:mt-[75px] lg:mt-[85px] 
           xl:mt-[95px] 2xl:mt-[100px] 3xl:mt-[105px]
           `}
+          style={{
+            backgroundPosition: "center center",
+            backgroundSize: "center",
+            objectFit: "cover",
+            width: "100%",
+            height: "50%",
+          }}
           src={imageSrc}
           alt="banner"
           title="Banner"
